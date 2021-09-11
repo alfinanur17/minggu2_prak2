34017da0aa9e5e3c76a8ff0a5af13416494237d9
alfinanur17 <alfinanur0117@gmail.com>
2021-09-11 17:12:40 +0700

    menambah

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        return "Selamat Datang";
+    }
+
+    public function about() {
+        return "Nama: Arifatun Nisa<br> NIM : 2031710064";
+    }
+
+    public function articles($id) {
+        return "Halaman Artikel dengan Id '$id'";
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +21,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles/{id}', [PageController::class,'articles']);
