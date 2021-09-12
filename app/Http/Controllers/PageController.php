37759664dd898988dc25e5a81a197c84d1f2b5cc
alfinanur17 <alfinanur0117@gmail.com>
2021-09-12 19:03:28 +0700

    menambah

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,7 +11,7 @@ class PageController extends Controller
     }
 
     public function about() {
-        return "Nama: Arifatun Nisa<br> NIM : 2031710064";
+        return "Nama : Alfian Nur A<br> NIM : 2031710064";
     }
 
     public function articles($id) {
